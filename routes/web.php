56249a6a99f37c551480bdb91171aab11f3b2398
Cindy Anggriani <xxcindyagrn@gmail.com>
2025-10-01 20:39:31 +0700

    view laravel

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Hello Laravel";
+    return view('index');
 });
 
 Route::get('/about', function () {
-    return "Ini adalah halaman <b>About</b>";
+    $nama = 'Cindy Anggriani';
+    return view('about')->with('nama',$nama);
 });
 
 Route::get('/mahasiswa', function () {
-    return "ini adalah halaman <b>Mahasiswa</b>";
+    return view('mahasiswa');
 });
 
 Route::get('/profile', function () {
-    $nama = "Cindy Anggriani";
-    return "ini adalah halaman <b>Profile</b>. Nama saya adalah " . $nama;
+    return view('profile');
 });
